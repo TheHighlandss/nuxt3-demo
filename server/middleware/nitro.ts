5b13bc503ec7311247c1bad70e0739727c8b8bdb
yintao <yintao@addcn.com>
2024-01-24 18:34:23 +0800

    nitro error handler

---
+++ server/middleware/nitro.ts
@@ -0,0 +1,18 @@
+import { parseURLQuery, stringifyURLQuery  } from "@t5-utils/core"
+
+export default defineNitroErrorHandler((error:any, event:any) => {
+    const originUrl = error.req.url
+
+    const query: Record<string, string> = parseURLQuery(originUrl)
+    const path = originUrl.split('?')[0]
+
+    if(+query.csr !== 1){
+      query.csr = '1'
+      const newQuery = stringifyURLQuery(query)
+      const url = `${path}?${newQuery}`
+
+      error.res.statusCode = 302;
+      error.res.setHeader('Location', url);
+      error.res.end();
+    }
+  })
